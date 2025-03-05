a2f28f2a1c85d6bc701acfe649198c83b417bb53
miccayo <6856974+miccayo@users.noreply.github.com>
2025-03-04 22:33:22 -0600

    Implement basic landing page

--- routes/site.ts
+++ routes/site.ts
@@ -3,7 +3,13 @@ const siteRoutes = express.Router();
 
 // Import API routes
 import { usersPages } from './site/users.ts';
+
 // Set routes
 siteRoutes.use('/users', usersPages);
+
+// Index page
+siteRoutes.get('/', (req, res) => {
+  res.render('landing', { title: 'Home' });
+});
 
 export { siteRoutes };
